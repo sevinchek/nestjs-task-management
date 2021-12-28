e7e769278aef7c0f8b126417c7a370e2f16ad01f
Kevin <kevinsevinche@gmail.com>
2021-12-28 09:56:57 -0500

    Setup database connection parameters

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+require('dotenv').config();
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: '192.168.0.110',
+  port: 5432,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: 'taskmanagement',
+  entities: [__dirname + '/../**/*.entity.ts'],
+  synchronize: true,
+};
