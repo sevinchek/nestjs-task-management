be67a9d88c222670c114bd088e103e671cb0a30a
Kevin <kevinsevinche@gmail.com>
2021-12-29 17:59:27 -0500

    Add authorization for get tasks endpoint

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -7,9 +7,11 @@ import { Task } from './task.entity';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
+
+    query.where('task.userId = :userId', { userId: user.id });
 
     if (status) {
       query.andWhere('task.status = :status', { status });
@@ -33,6 +35,7 @@ export class TaskRepository extends Repository<Task> {
     task.title = title;
     task.description = description;
     task.status = TaskStatus.OPEN;
+
     task.user = user;
     await task.save();
 
